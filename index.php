3b8bcf299dea07e1cf102eab63d13fd777e1cdf2
SpectreSpect <spectrefromthefuture@gmail.com>
2022-05-04 15:39:39 +0400

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>MLDM-Lab3-IsRelationFunction</title>
+    <link rel = "stylesheet" href = "css/index.css">
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <script src = "js/index.js">
+</body>
+</html>
